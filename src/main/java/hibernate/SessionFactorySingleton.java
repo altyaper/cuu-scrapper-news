54bfe16da7649e9a1308bf891bd09fcca0323572
Jorge Chavez <jchavez@switchfly.com>
2018-01-22 05:19:23 +0000

    Add env vars to hibernate

--- src/main/java/hibernate/SessionFactorySingleton.java
+++ src/main/java/hibernate/SessionFactorySingleton.java
@@ -12,7 +12,24 @@ public class SessionFactorySingleton {
 
     public static SessionFactory getInstance() {
         if(instance == null) {
-            instance = new Configuration().configure().buildSessionFactory();
+            Configuration cfg = new Configuration();
+            String DATABASE_URL = "";
+            String DATABASE_USER = "";
+            String DATABASE_PASSWORD = "";
+            String env = System.getenv("HIBERNATE_ENVIRONMENT");
+            if(env.equals("development")) {
+                DATABASE_URL = System.getenv("HIBERNATE_DATABASE_URL");
+                DATABASE_USER = System.getenv("HIBERNATE_DATABASE_USER");
+                DATABASE_PASSWORD = System.getenv("HIBERNATE_DATABASE_PASSWORD");
+            } else if(env.equals("production")) {
+                DATABASE_URL = System.getenv("PRODUCTION_HIBERNATE_DATABASE_URL");
+                DATABASE_USER = System.getenv("PRODUCTION_HIBERNATE_DATABASE_USER");
+                DATABASE_PASSWORD = System.getenv("PRODUCTION_HIBERNATE_DATABASE_PASSWORD");
+            }
+            cfg.setProperty("hibernate.connection.url", DATABASE_URL);
+            cfg.setProperty("hibernate.connection.username", DATABASE_USER);
+            cfg.setProperty("hibernate.connection.password", DATABASE_PASSWORD);
+            instance = cfg.configure().buildSessionFactory();
         }
         return instance;
     }
